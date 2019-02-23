c8d1d56ad8514bd7c2b545b1fec98f630c336b4c
aidanlw17 <aidan.lw17@gmail.com>
2019-02-23 13:47:39 -0500

    move validation

--- chess.py
+++ chess.py
@@ -40,18 +40,100 @@ def GetPlayerPositions(board, player):
     return positions
         
 def GetPieceLegalMoves(board,position):
+    #find which team the piece at the position belongs to
+    #determine which board positions are under threat by other team
+    #positions not under threat and available for the type of piece are legal moves
     piece = board[position]
+    pieces = {
+        10: WPawnLM,
+        11: WKnightLM,
+        # 12: WBishopLM,
+        # 13: WRookLM,
+        # 14: WQueenLM,
+        # 15: WKingLM,
+        20: BPawnLM,
+        21: BKnightLM,
+        # 22: BBishopLM,
+        # 23: BRookLM,
+        # 24: BQueenLM,
+        # 25: BKingLM
+    }
+    return pieces[piece](board,position)
+
+def WPawnLM(board, position):
+    moves=[]
+    #starting two square jump
+    if(position<=15 and board[position+16]==0):
+        moves+= [position+16]
+
+    #next square
+    if(position<56 and board[position+8]==0):
+        moves += [position +8]
+    
+    #diagonal squares
+    if(position<56 and position%8!=0 and position%7!=0):
+        if(board[position+7]>=20):
+            moves+=[position+7]
+        if(board[position+9]>=20):
+            moves+=[position+9]
+    elif(position<56 and position%8==0):
+        if(board[position+9]>=20):
+            moves+=[position+9]
+    elif(position<56 and position%7==0):
+        if(board[position+7]>=20):
+            moves+=[position+7]
+    return moves
+
+def BPawnLM(board,position):
+    moves=[]
+    #starting two square jump
+    if(position>=48 and board[position-16]==0):
+        moves+= [position-16]
+
+    #next square
+    if(position>7 and board[position-8]==0):
+        moves += [position-8]    
+
+    #diagonal squares
+    if(position>7 and position%8!=0 and position%7!=0):
+        if(board[position-7]<16 and board[position-7]>0):
+            moves+=[position-7]
+        if(board[position-9]<16 and board[position-9]>0):
+            moves+=[position-9]
+    elif(position>7 and position%8==0):
+        if(board[position-7]<16 and board[position-7]>0):
+            moves+=[position-7]
+    elif(position>7 and position%7==0):
+        if(board[position-9]<16 and board[position-9]>0):
+            moves+=[position-9]
+    return moves
+
+def WKnightLM(board,position):
+    moves=[]
+    a = [-10,-17,6,15,17,10,-6,-15]
+    if(position%8==0):
+        for p in a[4:]:
+            if((position+p)>=0 and (position+p)<64):
+                if(position)
+    elif(position%7==0):
+
+
+
 
 def main():
     #print and generate the board
     board = GenBoard()
     PrintBoard(board)
-
+    
     #PLAYER positions
     print("white positions: \n")
     print(GetPlayerPositions(board,10))
     print("black positions: \n")
     print(GetPlayerPositions(board,20))
+
+    #get piece legal moves
+    moves = GetPieceLegalMoves(board,49)
+    print(moves)
 
 main()
 
